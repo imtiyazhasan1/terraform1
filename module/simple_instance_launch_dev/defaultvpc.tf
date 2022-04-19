73b52e350c14789d92ee761fbd105060435df146
root <root@jump.localdomain>
2022-04-19 11:26:50 -0700

    module

--- module/simple_instance_launch_dev/defaultvpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "imtiyazvpc" {
-   cidr_block = "10.0.0.0/16"
-   tags = {
-      name = "Default VPC"
-}
-}
--- module/simple_instance_launch_dev/instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "test" {
-     ami = "ami-04a223c5825a793bc"
-     instance_type = "t2.micro"
-   tags = { 
-    name = "test"
-    environment = "prod"
-    security = "I-Z"
-} 
-}
